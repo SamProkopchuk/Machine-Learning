24b677f23d82dbda6724e84228efc168d3305b37
Sam Prokopchuk <ssprokopchuk@gmail.com>
2020-09-07 23:51:33 -0700

    Succesfully generating chars given start string (using argmax)

--- 2020.09/lstm_swesb_text_gen.py
+++ 2020.09/lstm_swesb_text_gen.py
@@ -64,22 +64,20 @@ def get_dataset_and_info(file_path):
     char_dataset = tf.data.Dataset.from_tensor_slices(text_as_ids)
     char_sequences = char_dataset.batch(SEQUENCE_LEN + 1, drop_remainder=True)
     dataset = char_sequences.map(seq_to_supervised)
-
     info = {
         'vocab': vocab,
         'char2id': char2id,
         'id2char': id2char,
         'text_as_ids': text_as_ids
     }
-
     return dataset, info
 
 
-def build_model(vocab_size):
+def build_model(vocab_size, batch_size=BATCH_SIZE):
     model = Sequential()
     model.add(Embedding(
         vocab_size, EMBEDDING_DIM,
-        batch_input_shape=[BATCH_SIZE, None]))
+        batch_input_shape=[batch_size, None]))
     model.add(LSTM(
         RNN_UNITS,
         return_sequences=True,
@@ -88,27 +86,50 @@ def build_model(vocab_size):
     return model
 
 
+def generate_text(model, start_string, char2id, id2char, num_generate=1000):
+    generated_text = ''
+    model.reset_states()
+    char_ids = tf.expand_dims(
+        [char2id[c] for c in start_string], axis=0)
+    for i in range(num_generate):
+        predictions = model.predict(char_ids)
+        next_prediction = tf.squeeze(predictions, axis=0)[-1]
+        pred_id = tf.math.argmax(next_prediction)
+        generated_text += id2char[pred_id]
+        char_ids = tf.expand_dims(pred_id, axis=0)
+    return start_string + generated_text
+
+
 def main():
     ds, info = get_dataset_and_info('./data/sw_esb_4th.txt')
     # Process sets of SEQUENCE_LEN, w/ batch size of BATCH_SIZE.
     # Eg, if SEQUENCE_LEN = 100 and BATCH_SIZE = 32,
     # a batch sample will have shape (32,100).
     ds = ds.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-    for X, Y in ds.take(1):
-        print(X.numpy().shape)
-        print(Y.numpy().shape)
     vocab, char2id, id2char, text_as_ids = (
         info['vocab'],
         info['char2id'],
         info['id2char'],
         info['text_as_ids'])
+    vocab_size = len(vocab)
 
-    model = build_model(len(vocab))
+    model = build_model(vocab_size)
 
     opt = tf.keras.optimizers.RMSprop()
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer=opt, loss=loss_fn, metrics=['accuracy'])
-    history = model.fit(ds, epochs=EPOCHS)
+
+    # Train the model.
+    model.fit(ds, epochs=EPOCHS)
+
+    # We will generate text character by character.
+    # This means we must build a model accepting a batch size of 1,
+    # using the weights from the trained model.
+    gen_model = build_model(vocab_size, batch_size=1)
+    gen_model.set_weights(model.get_weights())
+    del model
+    res = generate_text(gen_model, 'CHEWY: ', char2id, id2char)
+    print(res)
 
 
 if __name__ == '__main__':
