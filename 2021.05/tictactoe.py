4e1edf8f6337cb4cbb57a4a2d423f284ca8ab1d5
Sam Prokopchuk <ssprokopchuk@gmail.com>
2021-05-04 19:59:45 -0400

    Fix rng bug and PEP8

--- 2021.05/tictactoe.py
+++ 2021.05/tictactoe.py
@@ -9,15 +9,11 @@ The model should output a real value between -1 and 1
 given a board position and who's move it is.
 (assuming the previous move didn't win the game).
 
-To avoid repeated games, the order in which moves are considered will be randomized.
-
 The architecture:
 3x3 nodes for player 1's pieces
 3x3 nodes for player 2's pieces
 1 node for who's move it is.
 '''
-
-
 import numpy as np
 import tensorflow as tf
 
@@ -59,65 +55,46 @@ class MiniMaxAI:
     Assumes player 0 maximizes and player 1 minimizes
     '''
 
-    def __init__(self, num, evaluationFunction, maxDepth=4):
+    def __init__(self, num, evaluation_function, max_depth=4):
         self.num = num
-        self._f = evaluationFunction
-        self.maxDepth = maxDepth
-        self.moves = np.arange(9)
+        self._f = evaluation_function
+        self.max_depth = max_depth
 
     def minimax(self, board, p, depth, alpha, beta) -> Tuple[float, Optional[int]]:
-        if depth >= self.maxDepth:
-            return self._f(board, p), None
+        if Game.isWinner(board, 1-p):
+            return (1 if (1-p == 0) else -1), None
         elif board.sum() == 9:
             # Board is full
             return 0, None
-        elif p == 0:
-            # Is maximizer
-            bestWeight = -INF
-            bestMove = None
-            np.random.shuffle(self.moves)
-            for move in self.moves:
+        elif depth >= self.max_depth:
+            return self._f(board, p), None
+        else:
+            best_weight = INF if p else -INF
+            best_move = None
+            for move in range(9):
                 r, c = divmod(move, 3)
                 if board[r][c].any():
                     continue
                 board[r][c][p] = 1
-                if Game.isWinner(board, p):
-                    board[r][c][p] = 0
-                    return 1, move
                 weight, _ = self.minimax(board, 1-p, depth+1, alpha, beta)
-                if weight > bestWeight:
-                    bestWeight = weight
-                    bestMove = move
                 board[r][c][p] = 0
-                alpha = max(alpha, bestWeight)
+                if p:
+                    if weight < best_weight:
+                        best_weight = weight
+                        best_move = move
+                    beta = min(beta, best_weight)
+                else:
+                    if weight > best_weight:
+                        best_weight = weight
+                        best_move = move
+                    alpha = max(alpha, best_weight)
                 if alpha >= beta:
                     break
-            return bestWeight, bestMove
-        else:
-            # Is minimizer
-            bestWeight = INF
-            bestMove = None
-            np.random.shuffle(self.moves)
-            for move in self.moves:
-                r, c = divmod(move, 3)
-                if board[r][c].any():
-                    continue
-                board[r][c][p] = 1
-                if Game.isWinner(board, p):
-                    board[r][c][p] = 0
-                    return -1, move
-                weight, _ = self.minimax(board, 1-p, depth+1, alpha, beta)
-                if weight < bestWeight:
-                    bestWeight = weight
-                    bestMove = move
-                board[r][c][p] = 0
-                beta = min(beta, bestWeight)
-                if beta <= alpha:
-                    break
-            return bestWeight, bestMove
+            return best_weight, best_move
 
-    def getMove(self, board):
-        _, move = self.minimax(board, self.num, 0, -INF, INF)
+    def get_move(self, board):
+        weight, move = self.minimax(board, self.num, 0, -INF, INF)
+        # print(f'Current evaluation is {weight}')
         return move
 
 
@@ -125,7 +102,7 @@ class CLIPlayer:
     def __init__(self, num):
         self.num = num
 
-    def getMove(self, board):
+    def get_move(self, board):
         move = -1
         while not (0 <= move < 9) or board[divmod(move, 3)].any():
             try:
@@ -136,11 +113,12 @@ class CLIPlayer:
                 continue
         return move
 
+
 class Game:
-    def __init__(self, playerOne, playerTwo):
+    def __init__(self, player1, player2):
         self.board = np.zeros((3, 3, 2))
-        self.playerOne = playerOne
-        self.playerTwo = playerTwo
+        self.player1 = player1
+        self.player2 = player2
 
     @staticmethod
     def isWinner(board: np.array, p):
@@ -148,7 +126,6 @@ class Game:
         return any(B[i].all() for i in range(B.shape[0])) or \
             any(B.T[i].all() for i in range(B.shape[1])) or \
             B.diagonal().all() or np.flipud(B).diagonal().all()
-        return res
 
     def print_board(self):
         for r in range(3):
@@ -162,32 +139,33 @@ class Game:
             print('|')
 
     def play(self, verbose=False):
-        gameHistory = {'winner': None, 'moves': []}
+        game_history = {'winner': None, 'moves': []}
         p = 0
         for _ in range(9):
             if p == 0:
-                move = self.playerOne.getMove(self.board)
+                move = self.player1.get_move(self.board)
             else:
-                move = self.playerTwo.getMove(self.board)
+                move = self.player2.get_move(self.board)
             row, col = divmod(move, 3)
             self.board[row][col][p] = 1
-            gameHistory['moves'].append(move)
+            game_history['moves'].append(move)
             if verbose:
                 print(f'Player {p+1} at row {row+1} column {col+1}')
                 self.print_board()
             if Game.isWinner(self.board, p):
-                gameHistory['winner'] = p
+                game_history['winner'] = p
                 break
             p = 1-p
-        return gameHistory
+        return game_history
 
 
 def main():
-    evaluationFunction = EvaluationFunction()
-    playerOne = MiniMaxAI(0, evaluationFunction, maxDepth=4)
-    playerTwo = CLIPlayer(1)
-    game = Game(playerOne, playerTwo)
-    game.play(verbose=True)
+    evaluation_function = EvaluationFunction()
+    player1 = MiniMaxAI(0, evaluation_function, max_depth=5)
+    player2 = MiniMaxAI(1, evaluation_function, max_depth=5)
+    game = Game(player1, player2)
+    game_history = game.play(verbose=True)
+    print(game_history)
 
 
 if __name__ == '__main__':
