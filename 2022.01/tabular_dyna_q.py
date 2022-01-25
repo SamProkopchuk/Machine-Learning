d7a652599d24d4a9f51a64b4582a7e752e9794c2
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-01-25 00:09:50 -0500

    Working code very nice

--- 2022.01/tabular_dyna_q.py
+++ 2022.01/tabular_dyna_q.py
@@ -27,20 +27,31 @@ S#X####X#
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import product
 from tqdm import tqdm
+# Efficiency ez clap:
+from bisect import bisect_left, insort
 
-_STATES = list(set(range(6 * 9)) - {7, 16, 25, 11, 20, 29, 41})
+# Exclude the terminal state 8 and walls:
+_STATES = list(set(range(6 * 9)) - {7, 8, 16, 25, 11, 20, 29, 41})
 _ACTIONS = [*range(4)]
 
 
 def environment(s, a) -> int:
     # Imagine making a readable function WeirdChamp
     if a == 0:
-        return (s if s % 9 == 8 or s in {10, 19, 28, 6, 15, 24, 40} else s + 1), 0
+        return (
+            s if s %
+            9 == 8 or s in {
+                10, 19, 28, 6, 15, 24, 40} else s + 1), 0
     elif a == 1:
-        return (s if s < 9 or s in {34, 38, 50} else s - 9), (1 if s == 17 else 0)
+        return (s if s < 9 or s in {34, 38, 50}
+                else s - 9), (1 if s == 17 else 0)
     elif a == 2:
-        return (s if s % 9 == 0 or s in {8, 12, 17, 21, 26, 30, 42} else s - 1), 0
+        return (
+            s if s %
+            9 == 0 or s in {
+                8, 12, 17, 21, 26, 30, 42} else s - 1), 0
     else:  # a == 3
         return (s if s > 44 or s in {2, 32} else s + 9), 0
 
@@ -54,24 +65,47 @@ def environment(s, a) -> int:
 #         plt.show()
 
 
-def main(trials=700000, lmbda=1, alpha=5e-3, epsilon=1e-1, n=10):
-    action_values = {(s, a): 1 for s, a in product(_STATES, _ACTIONS)}
-    # Exaggerate initial expected action-reward to help exploration
+def sorted_list_contains(l, x):
+    i = bisect_left(l, x)
+    return i != len(l) and l[i] == x
+
+
+def insort_if_not_contains(l, x):
+    if not sorted_list_contains(l, x):
+        insort(l, x)
+
+
+def main(trials=1000, lmbda=1, alpha=1e-1, epsilon=2e-1, n=50):
+    action_to_not = {a: [x for x in _ACTIONS if x != a] for a in _ACTIONS}
+    action_values = {(s, a): 1 for s, a in product(_STATES, _ACTIONS)} | {(8, a): 1 for a in _ACTIONS}
+    model = {(s, a): None for s, a in product(_STATES, _ACTIONS)}
+    seen_s = []
+    seen_sa = []
 
     for _ in tqdm(range(trials)):
         s = random.choice(_STATES)
-        while s:
-            a = max(_ACTIONS, key=lambda x: action_values[s, x])
-            if random.random() < epsilon:
-                a = not a
-            r, sprime = environment(s, a)
-            # Now we have s, a, r, sprime
+        a = max(_ACTIONS, key=lambda x: action_values[s, x])
+        if random.random() < epsilon:
+            a = random.choice(action_to_not[a])
+
+        sprime, r = environment(s, a)
+        action_values[s,
+                      a] += alpha * (r + lmbda * max(action_values[sprime,
+                                                                   _a] for _a in _ACTIONS) - action_values[s,
+                                                                                                           a])
+        model[s, a] = (sprime, r)
+        insort_if_not_contains(seen_s, s)
+        insort_if_not_contains(seen_sa, (s, a))
+
+        for __ in range(n):
+            s = random.choice(seen_s)
+            a = random.choice([a for a in _ACTIONS if model[s, a] is not None])
+
+            r, sprime = model[s, a]
             action_values[s,
                           a] += alpha * (r + lmbda * max(action_values[sprime,
                                                                        _a] for _a in _ACTIONS) - action_values[s,
                                                                                                                a])
-            s = sprime
-
 
 
 if __name__ == '__main__':
