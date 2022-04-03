b9c11ee99761817d9ddf14b5dd6c842b77d2078f
Sam Prokopchuk <ssprokopchuk@gmail.com>
2022-04-03 00:57:12 -0400

    Update tile.py

--- 2022.03/sarsa_regression.py
+++ 2022.03/sarsa_regression.py
@@ -1,17 +1,19 @@
 '''
-sarsa_sav_function_approximation
-
-=>
-
-sarsa state-action value function approximation
-
+The moutain car problem
+Solved via sarsa linear regression
 '''
-from math import cos
+import random
+
 import numpy as np
+
+from math import cos
+
+
+# Params you can f with:
 
 _EPSILON = 1e-10
 
-_ACTIONS = {-1, 0, 1}
+_ACTIONS = [-1, 0, 1]
 
 _POS_BOUNDS = (-1.2, 0.6)
 _VEL_BOUNDS = (-0.7, 0.7)
@@ -21,11 +23,12 @@ _VEL_BUCKETS = 8
 
 _AXIS_SHIFTS = 4
 
-######################
+# Params you can't f with:
 
 N_BUCKETS_PER_TILE = _POS_BUCKETS * _VEL_BUCKETS
 N_TILES = 1 + (4 * _AXIS_SHIFTS)
 N_BUCKETS = N_BUCKETS_PER_TILE * N_TILES
+X_SIZE = len(_ACTIONS) * N_BUCKETS
 
 
 def clip(a, a_min, a_max):
@@ -33,14 +36,19 @@ def clip(a, a_min, a_max):
     return min(a_max, max(a_min, a))
 
 
-def state_to_X(pos, vel):
+def get_dimensional_tiling_index(x_min, x_max, axis_tiles, n_axis_tiles):
+    pass
+
+
+def state_action_to_x(pos, vel, a):
     '''
     Idea:
     We start at some center tile
     and stride in combinations of (-1, 1) x y directions
     such that all strides are evenly spaced out
     '''
-    res = np.zeros(N_BUCKETS)
+    res = np.zeros((X_SIZE, 1))
+    off = _ACTIONS.index(a) * N_BUCKETS
     pos_base_ubs = np.linspace(*_POS_BOUNDS, _POS_BUCKETS + 1)[1:]
     vel_base_ubs = np.linspace(*_VEL_BOUNDS, _VEL_BUCKETS + 1)[1:]
 
@@ -52,22 +60,53 @@ def state_to_X(pos, vel):
             stride = axis_shift / (_AXIS_SHIFTS + 1)
             pos_idx = np.searchsorted(pos_ubs + dx * stride, pos)
             vel_idx = np.searchsorted(vel_ubs + dy * stride, vel)
-            res[i * N_BUCKETS_PER_TILE + pos_idx + vel_idx * _POS_BUCKETS] = 1
+            res[off + i * N_BUCKETS_PER_TILE + pos_idx + vel_idx * _POS_BUCKETS] = 1
             i += 1
     return res
 
 
-def next_state(curr_pos, curr_vel, a):
-    return clip(curr_pos + curr_vel, _POS_BOUNDS)
+def next_state(s, a):
+    next_pos = clip(curr_pos + curr_vel, *_POS_BOUNDS)
+    next_vel = 0 if next_pos = _POS_BOUNDS[0] else clip(curr_vel + 0.001 * a - 0.0025 * cos(3 * curr_pos), *_VEL_BOUNDS)
+    return next_pos, next_vel
 
 
-def next_velocity(curr_pos, curr_vel, a):
-    return clip(curr_vel + 0.001 * a - 0.0025 *
-                cos(3 * curr_pos), *_VEL_BOUNDS)
+def environment(s, a):
+    return -1, next_state(s, a)
 
 
-def main():
-    pass
+def state_action_value(x, w):
+    return w.T.dot(x)
+
+
+def best_action(pos, vel, w):
+    x = state_action_to_x(pos, vel, a)
+    w = state_action_value(x, w)
+    return max(_ACTIONS, key=lambda a: state_action_value(x, w))
+
+
+def is_terminal_state(s):
+    return s[0] == _POS_BOUNDS[1]
+
+
+def main(trials=10000, lmda=1, alpha=1e-2, epsilon=1e-1):
+    w = np.zeros((N_BUCKETS, 1))
+    action_to_not = {a: [x for x in _ACTIONS if x != a] for a in _ACTIONS}
+
+    for _ in range(trials):
+        s = [random.uniform(*_POS_BOUNDS), random.uniform(*_VEL_BOUNDS)]
+        a = best_action(*s, w)
+        if random.random() < epsilon:
+            a = random.choice(action_to_not[a])
+        if not is_terminal_state(s): # Basically 0% chance of being false but whatever
+            while True :
+                r, sprime = environment(s, a)
+                if is_terminal_state(sprime):
+                    w += alpha * (...)
+                    break
+                aprime = best_action(*sprime, w)
+                w += alpha * (...)
+                s, a = sprime, aprime
 
 
 if __name__ == '__main__':
--- 2022.03/tile.py
+++ 2022.03/tile.py
@@ -1,46 +1,121 @@
 import numpy as np
+from pandas.util import hash_array
+
+EPSILON = 1e-6
 
 
-def discretize(features, feat_ranges, buckets_per_feat):
+def discretize(features, feat_ranges, buckets_per_feat, n, i):
+    '''
+    features: shape [f]
+    feat_ranges: shape [f, 2]
+    buckets_per_feat: number of buckets along each feature axis
+    k: n_buckets for a single feat
+    n: n_tilings
+    i: which tiling [0, n-1] it is
+    We use the equation:
+    int( [ ((f-a) / (b-a)) * kn/(n(k-1)+1) + i/kn ] * buckets_per_feat )
+    '''
+
     features, feat_ranges, buckets_per_feat = map(
-        np.asarray, (features, feat_ranges, buckets_per_feat))
-    features = (features - feat_ranges[:, 0]) / \
-        (feat_ranges[:, 1] - feat_ranges[:, 0]) * buckets_per_feat
+        np.asarray,
+        (features, feat_ranges, buckets_per_feat))
+    features = \
+        (
+            (features - feat_ranges[:, 0]) / (feat_ranges[:, 1] - feat_ranges[:, 0]) *
+            (n * (buckets_per_feat - 1) + 1) / (buckets_per_feat * n) +
+            i / (buckets_per_feat * n)) * \
+        buckets_per_feat - EPSILON
     return features.astype(int)
 
 
-def tile_encode(samples, feat_ranges, buckets_per_feat):
+def tile_encode(samples, feat_ranges, buckets_per_feat, n):
+    '''
+    samples: shape [n_samples, f]
+    feat_ranges: shape [m, f]
+    buckets_per_feat: number of buckets along each feature axis
+    n: n_tilings
+    '''
     samples, feat_ranges, buckets_per_feat = map(
-        np.asarray, (samples, feat_ranges, buckets_per_feat))
-    tile_shape = len(feat_ranges) * [buckets_per_feat] if buckets_per_feat.size == 1 else buckets_per_feat
-    res = np.zeros((len(samples), *tile_shape))
-    dis = np.apply_along_axis(discretize, 1, samples, feat_ranges, buckets_per_feat)
-    res[np.arange(len(samples))[...], dis[..., 0], dis[..., 1]] = 1
+        np.asarray, 
+        (samples, feat_ranges, buckets_per_feat))
+    res = np.zeros((len(samples), n, np.product(buckets_per_feat)))
+    tilemul = np.arange(n).reshape(-1, 1)
+    dis = np.array([
+        discretize(
+            sample,
+            feat_ranges,
+            buckets_per_feat,
+            n,
+            tilemul)
+        for sample in samples])
+    i, j = np.indices(dis.shape[:2])
+    res[
+        i.ravel(),
+        j.ravel(),
+        np.ravel_multi_index(
+            dis.reshape(
+                -1,
+                len(buckets_per_feat)).T,
+            buckets_per_feat)] = 1
     return res
 
 
-def hash_encode(samples, feat_ranges, buckets_per_feat, hash_table_size, hash_fn=hash):
+def hash_1darray(arr):
+    return np.ravel_multi_index()
+    return 
+
+
+def hash_encode(
+        samples,
+        feat_ranges,
+        buckets_per_feat,
+        n,
+        hash_table_size,
+        hash_fn=hash_1darray):
     samples, feat_ranges, buckets_per_feat = map(
-        np.asarray, (samples, feat_ranges, buckets_per_feat))
-    tile_shape = len(feat_ranges) * [buckets_per_feat] if buckets_per_feat.size == 1 else buckets_per_feat
-    res = np.zeros((len(samples), hash_table_size))
-    dis = np.apply_along_axis(discretize, 1, samples, feat_ranges, buckets_per_feat)
-    dis = np.ravel_multi_index([dis[..., 0], dis[..., 1]], tile_shape) % hash_table_size
-    res[np.arange(len(samples))[...], dis] = 1
-    return res
+        np.asarray,
+        (samples, feat_ranges, buckets_per_feat))
+    tilemul = np.arange(n).reshape(-1, 1)
+    dis = np.array([
+        hash_fn(
+            discretize(
+                sample,
+                feat_ranges,
+                buckets_per_feat,
+                n,
+                tilemul))
+        for sample in samples]) % hash_table_size
+    return dis
 
 
 def main():
+    import matplotlib.pyplot as plt
+    from itertools import product
+    from collections import defaultdict
+    n = 4
 
-    print(discretize([0, 1], [[0, 2], [0, 2]], 4))
+    print(hash_1darray(np.array([0, 1])))
+    print(hash_encode([[0, 0.25], [0.25, 0.75]], [[0, 1], [0, 1]], [3, 4], n, 69))
 
-    print('-' * 10)
+    import time
+    t = time.time()
+    X = tile_encode(np.random.uniform(0, 1, 300000).reshape(-1, 3), [[0, 1], [0, 1], [0, 1]], [7, 3, 4], n)
+    print(f'Took {time.time() - t:.3f} seconds')
+    print(X.shape)
+    for i in range(n):
+        buckets = defaultdict(list)
+        for x, y in product(np.linspace(0, 1, 48), np.linspace(0, 1, 48)):
+            bucket = hash(tuple(discretize([x, y], [[0, 1], [0, 1]], [7, 3], n, i)))
+            buckets[bucket].append((x, y))
 
-    print(tile_encode([[0.5, 1.4], [0, 0]], [[0, 2], [0, 2]], 4))
+        fig = plt.figure()
+        timer = fig.canvas.new_timer(interval=1000)
+        timer.add_callback(lambda: plt.close())
+        for bucket in buckets:
+            plt.scatter(*zip(*buckets[bucket]), label=bucket)
 
-    print('-' * 10)
-
-    print(hash_encode([[0.5, 1.4], [0, 0]], [[0, 2], [0, 2]], 4, 7))
+        timer.start()
+        plt.show()
 
 
 if __name__ == '__main__':
