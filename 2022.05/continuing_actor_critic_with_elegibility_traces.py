f37417a4020c67a1de1a0c19ece08c1b29f3c56d
Sam Prokopchuk <ssprokopchuk@gmail.com>
2023-03-20 14:50:51 -0400

    Update continueing actor-critic w/ elegibility traces for new pkg versions

--- 2022.05/continuing_actor_critic_with_elegibility_traces.py
+++ 2022.05/continuing_actor_critic_with_elegibility_traces.py
@@ -1,9 +1,9 @@
-'''
+"""
 The cart pole problem, continuous.
 Solved via Actor-Critic Algorithm with Elegibility Traces (continuing)
 The policy pi uses a neural-network function H. Thereafter applying softmax.
 The state-value v function is simple linear regression using tiling.
-'''
+"""
 import random
 import gym
 import numpy as np
@@ -13,25 +13,17 @@ from tqdm import tqdm
 from tile import tile_encode
 
 _NUM_ACTIONS = 2
-_STATE_SHAPE = (4, )
+_STATE_SHAPE = (4,)
 
 
-_BUCKETS_PER_FEAT = [
-    4,
-    4,
-    8,
-    8]
+_BUCKETS_PER_FEAT = [4, 4, 8, 8]
 
-_FEAT_RANGES = [
-    [-4.8, 4.8],
-    [-3, 3],
-    [-0.418, 0.418],
-    [-3, 3]]
+_FEAT_RANGES = [[-4.8, 4.8], [-3, 3], [-0.418, 0.418], [-3, 3]]
 _N_TILINGS = 8
 
 
-def clip_state(s, feat_ranges):
-    for i, fr in enumerate(feat_ranges):
+def clip_state(s):
+    for i, fr in enumerate(_FEAT_RANGES):
         s[i] = min(fr[1], max(fr[0], s[i]))
     return s
 
@@ -39,12 +31,15 @@ def clip_state(s, feat_ranges):
 class H(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.layers = torch.nn.ModuleList([
-            torch.nn.Linear(_STATE_SHAPE[0] * _NUM_ACTIONS, 32),
-            torch.nn.Linear(32, 1)])
-        self.activation_functions = torch.nn.ModuleList([
-            torch.nn.LeakyReLU(),
-            torch.nn.LeakyReLU()])
+        self.layers = torch.nn.ModuleList(
+            [
+                torch.nn.Linear(_STATE_SHAPE[0] * _NUM_ACTIONS, 32),
+                torch.nn.Linear(32, 1),
+            ]
+        )
+        self.activation_functions = torch.nn.ModuleList(
+            [torch.nn.LeakyReLU(), torch.nn.LeakyReLU()]
+        )
         self.init_weights()
 
     def init_weights(self):
@@ -52,14 +47,13 @@ class H(torch.nn.Module):
             torch.nn.init.kaiming_uniform_(layer.weight)
 
     def forward(self, x):
-        for layer, activation_function in zip(
-                self.layers, self.activation_functions):
+        for layer, activation_function in zip(self.layers, self.activation_functions):
             x = activation_function(layer(x))
         return x
 
     def __call__(self, s, a):
         x = np.zeros(_STATE_SHAPE[0] * _NUM_ACTIONS)
-        x[a * _STATE_SHAPE[0]: (a + 1) * _STATE_SHAPE[0]] = s
+        x[a * _STATE_SHAPE[0] : (a + 1) * _STATE_SHAPE[0]] = s
         x = torch.as_tensor(x, dtype=torch.float32)
         return super().__call__(x)
 
@@ -76,11 +70,11 @@ class Policy:
         return self.best_action(s)
 
     def get_action_with_p(self, s):
-        '''
+        """
         Return a tuple of two items:
             a: The action according to the policy
             p: The pytorch-differentiable probability of a
-        '''
+        """
         a2p = [self.h(s, a) for a in range(_NUM_ACTIONS)]
         ps = [p.item() for p in a2p]
         tot = sum(np.exp(p) for p in ps)
@@ -89,24 +83,25 @@ class Policy:
             a = random.choices(range(_NUM_ACTIONS), weights=weights)[0]
         except ValueError as e:
             from os import get_terminal_size
+
             c = get_terminal_size().columns
             print(e)
-            print('Debugging info:')
+            print("Debugging info:")
             print(a2p)
             print(ps)
             print(weights)
             print(tot)
-            print('-' * c)
+            print("-" * c)
             for param in self.h.parameters():
                 print(param.data)
-            print('-' * c)
+            print("-" * c)
             exit(0)
         return a, torch.exp(a2p[a]) / tot
 
     def best_action(self, s):
-        '''
+        """
         Return the action corresponding to the highest probability
-        '''
+        """
         return max(range(2), key=lambda a: self.h(s, a))
 
     def update_params(self, d, gradient):
@@ -134,12 +129,8 @@ class StateValueFunction:
         self.grad = None
 
     def __call__(self, s):
-        s = clip_state(s, _FEAT_RANGES)
-        te = tile_encode(
-            [s],
-            _FEAT_RANGES,
-            self.buckets_per_feat,
-            self.n_tiles)
+        s = clip_state(s)
+        te = tile_encode([s], _FEAT_RANGES, self.buckets_per_feat, self.n_tiles)
         te = te.ravel()
         self.grad = te
         return self.w.dot(te)
@@ -153,60 +144,55 @@ class StateValueFunction:
 
 
 def fit(pi, v, alpha=5e-3, trials=100_000):
-    '''
+    """
     Run the algorithm with:
         Policy pi
         State-value function v
     alpha: update rate for R
-    '''
+    """
     # R is estimate of the average reward
     R = 0
-    env = gym.make('CartPole-v1')
+    env = gym.make("CartPole-v1")
     env._max_episode_steps = np.inf
-    s = env.reset()
+    s, _ = env.reset()
     ep_lens = [0]
     for i in (pbar := tqdm(range(trials))):
         a, y = pi.get_action_with_p(s)
-        sprime, r, is_episode_done, _ = env.step(a)
+        sprime, r, is_episode_done, _, _ = env.step(a)
         d = r - R + v(sprime) - v(s)
         R += alpha * d
         v.update_params(d)
         pi.update_params(d, y)
         if is_episode_done:
-            pbar.set_description(f'Avg Episode Length: {sum(ep_lens[-10:]) / len(ep_lens[-10:])} {R}')
+            pbar.set_description(
+                f"Avg Episode Length: {sum(ep_lens[-10:]) / len(ep_lens[-10:])} {R}"
+            )
             ep_lens.append(0)
-            s = env.reset()
+            s, _ = env.reset()
         else:
             ep_lens[-1] += 1
             s = sprime
 
 
 def render_episodes(pi, nepisodes=3):
-    env = gym.make('CartPole-v1')
+    env = gym.make("CartPole-v1", render_mode="rgb_array")
     env._max_episode_steps = np.inf
     for _ in range(nepisodes):
-        s = env.reset()
+        s, _ = env.reset()
         is_episode_done = False
         while not is_episode_done:
             a = pi.best_action(s)
-            s, r, is_episode_done, d = env.step(a)
+            s, r, is_episode_done, d, _ = env.step(a)
             env.render()
     env.close()
 
 
 def main():
-    pi = Policy(
-        h_func=H().float(),
-        lmbda=0.80,
-        alpha=1e-1)
-    v = StateValueFunction(
-        _BUCKETS_PER_FEAT,
-        _N_TILINGS,
-        lmbda=0.80,
-        alpha=1e-2)
+    pi = Policy(h_func=H().float(), lmbda=0.80, alpha=1e-1)
+    v = StateValueFunction(_BUCKETS_PER_FEAT, _N_TILINGS, lmbda=0.80, alpha=1e-2)
     fit(pi, v)
     render_episodes(pi)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
